3ec4a2c58a8166a8050418d19fca31a74c57cd5c
Rkebede <rutht.kebede@gmail.com>
2021-02-18 15:53:07 -0600

    creates form component

--- tip-calculator/src/Form.js
+++ tip-calculator/src/Form.js
@@ -1,21 +1,34 @@
+import {useState} from 'react';
+
 const TipForm = () => {
 
+  const [bill, setBill] = useState(null)
+  const [service, setService] = useState(null)
+  const [numberOfPeople, setNumberOfPeople] = useState(null)
   return (
     <form>
-      <label>
-        How much was your bill?
-          <input type='text' ></input>
-      </label>
-      <label>
-        How was your service?
-        <input type='text' ></input>
-      </label>
-      <label>
-        How many people are sharing the bill?
-        <input type='text' ></input>
-
-      </label>
-
+      <div>
+        <label>How much was your bill?</label>
+      <input type='number' value={bill}></input>
+      </div>
+      <div>
+         <label>How was your service?</label>
+      <select id="service" name="service">
+        <option disabled selected>How did you feel about it</option>
+        <option value="30"> 😍 </option>
+        <option value="20"> 😯 </option>
+        <option value="18"> 🙂 </option>
+        <option value="15"> 🙁 </option>
+        <option value="10"> 😒 </option>
+        <option value="5"> 😣 </option>
+      </select>
+      </div>
+     <div>
+       <label>How many people are sharing the bill?</label>
+      <input type='number' value={numberOfPeople} ></input>
+     </div>
+      
+      <button>Calculate</button>
     </form>
   )
 }

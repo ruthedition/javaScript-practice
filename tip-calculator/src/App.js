f2c534fdaf2bdbb8536bd3afae4ae05f5e9b6bec
Rkebede <rutht.kebede@gmail.com>
2021-02-18 15:15:25 -0600

    addes form for bill input

---
+++ tip-calculator/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TipForm from './Form';
+
+function App() {
+  return (
+    <div>
+      <header>Tip Calculator</header>
+      <TipForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ tip-calculator/src/Form.js
@@ -0,0 +1,23 @@
+const TipForm = () => {
+
+  return (
+    <form>
+      <label>
+        How much was your bill?
+          <input type='text' ></input>
+      </label>
+      <label>
+        How was your service?
+        <input type='text' ></input>
+      </label>
+      <label>
+        How many people are sharing the bill?
+        <input type='text' ></input>
+
+      </label>
+
+    </form>
+  )
+}
+
+export default TipForm
